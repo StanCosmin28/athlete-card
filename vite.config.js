5dbf5feefcd82e6f7931a6e7295b79afcf908620
Stan Cosmin <stancosminioan@gmail.com>
2025-04-08 14:31:26 +0300

    Public project on GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/athlete-app/",
+  base: "/athlete-card/",
   plugins: [tailwindcss(), react()],
 });
