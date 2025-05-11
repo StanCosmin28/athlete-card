130ca7d30541543be40ba593383a53705294e605
Stan Cosmin <stancosminioan@gmail.com>
2025-05-11 21:09:12 +0300

    cleanup components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,9 +8,6 @@ import Medals from "./Components/Medals";
 import Sponsors from "./Components/Sponsors";
 import UsefulLinks from "./Components/UsefulLinks";
 import Story from "./Components/Story";
-// import Highlights from "./Components/Highlights";
-// import MedalWall from "./Components/MedalWall";
-// import ImageTest from "./Components/ImageTest";
 import FixedBackground from "./Components/FixedBackground";
 import MedalsV2 from "./Components/MedalsV2";
 import StoryV2 from "./Components/StoryV2";
@@ -18,7 +15,6 @@ import UsefulLinksV2 from "./Components/UsefulLinksV2";
 import SponsorsV2 from "./Components/SponsorsV2";
 import FixedBackgroundV2 from "./Components/FixedBackgroundV2";
 import Copyrights from "./Components/Copyrights";
-// import DockLinks from "./Components/DockLinks";
 import { useState } from "react";
 import AthleteBadge from "./Components/AthleteBadge";
 import WaitlistPage from "./Components/Waitlist";
@@ -88,11 +84,7 @@ function App() {
                 <div className="max-w-7xl mx-auto px-4 py-4">
                   {simple ? <StoryV2 /> : <Story />}
                 </div>
-                {/* <div className="max-w-7xl mx-auto px-4 py-4">
-                  <ImageTest />
-                </div> */}
                 <Copyrights />
-                {/* <DockLinks /> */}
               </DataProvider>
             </>
           }
